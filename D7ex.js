fae5b1d261015a7cdcb9cd2246fb90628bdbe35c
tomas berhane <tomib@tomass-MacBook-Air.local>
2021-05-25 22:46:10 +0200

    solved exercise 7,8 and 9

--- D7ex.js
+++ D7ex.js
@@ -1,11 +1,23 @@
+//ex.7
 function change(){
     const h1 = document.querySelector('h1')
     h1.innerText = 'not opening today'
 
 }
 
+//ex.8
 function changeBackground(){
     const Bg = document.querySelector('body')
+    Bg.classList.toggle('selected')
      Bg.style.backgroundColor = 'brown'
+
 }
 
+//ex.9
+
+function changeFooter(){
+    let foot = document.querySelector('footer')
+    foot.innerText = 'james@gmbh-rel.com'
+}
+
+//ex.10
