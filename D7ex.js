79fb73621bb7dd0484da3d41a2bbb220c255b4b1
tomas berhane <tomib@tomass-MacBook-Air.local>
2021-05-31 16:48:07 +0200

    solved ex 10 and 11

--- D7ex.js
+++ D7ex.js
@@ -1,23 +1,47 @@
-//ex.7
+// EX7.: Write a function to change the h1 text
 function change(){
     const h1 = document.querySelector('h1')
     h1.innerText = 'not opening today'
 
 }
 
-//ex.8
+// EX8.: Write a function to change the page background color
 function changeBackground(){
     const Bg = document.querySelector('body')
-    Bg.classList.toggle('selected')
-     Bg.style.backgroundColor = 'brown'
+     Bg.style.backgroundColor = 'green'
+    
+     
 
 }
 
-//ex.9
+// EX9.: Write a function to change the footer address with a fake one
 
 function changeFooter(){
     let foot = document.querySelector('footer')
     foot.innerText = 'james@gmbh-rel.com'
 }
 
-//ex.10
+// EX10.: Write a function to add a CSS class to every Amazon link
+function addCss(){
+    for( let node of document.querySelectorAll('a')){
+        node.classList.add('amazon-link')
+    }
+}
+
+
+// EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image
+
+
+function toggleImage(){
+    for(let node of document.querySelectorAll('img')){
+        
+        node.classList.toggle('hidden')
+    }
+    
+}
+
+
+// EX12: Write a function to color the price of each product in a different one every time it's invoked
+function changeColor(){
+
+}
